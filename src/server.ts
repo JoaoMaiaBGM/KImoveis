5bce1b3291afc7f3d2b6084def6dc33d51bbfbe1
João Maia <joaocarlosbgm@gmail.com>
2022-10-19 11:36:18 -0300

    refactor: server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ import app from "./app";
 import AppDataSource from "./data-source";
 
 (async () => {
+  const PORT = process.env.PORT || 3000;
 
-    await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-    
-    app.listen(3000, () => {
-        console.log("Servidor executando")
-    })    
-})()
+  await AppDataSource.initialize().catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Servidor executando na porta ${PORT}`);
+  });
+})();
